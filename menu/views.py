effb3d7a553691b650cc2a77ee4f2bc228818016
flit97 <flittotech@gmail.com>
2025-06-12 16:39:42 -0400

    Premier commit

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pizza
 
 
 def index(request):
-    return HttpResponse("Les pizzas")
+    '''pizzas = Pizza.objects.all()
+    pizza_names_and_price = [pizza.nom + " : " + str(pizza.prix) + "€" for pizza in pizzas]
+    pizza_names_string = ", ".join(pizza_names_and_price)
+    return HttpResponse("Les pizzas : " + pizza_names_string)'''
+    return render(request, 'menu/index.htm')
+
+
